741ab1e6dc1c23ab55ba46bcfe3e8c7ec2e7dc48
Ionos Cloud SDK Robot <sdk@cloud.ionos.com>
2024-10-09 14:50:20 +0000

    auto-generated version 1.2.0

--- configuration.go
+++ configuration.go
@@ -138,7 +138,7 @@ func NewConfiguration(username, password, token, hostUrl string) *Configuration 
 	cfg := &Configuration{
 		DefaultHeader:      make(map[string]string),
 		DefaultQueryParams: url.Values{},
-		UserAgent:          "ionos-cloud-sdk-go-logging/v1.1.0",
+		UserAgent:          "ionos-cloud-sdk-go-logging/v1.2.0",
 		Debug:              false,
 		Username:           username,
 		Password:           password,
